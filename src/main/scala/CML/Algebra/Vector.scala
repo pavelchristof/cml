685b67d0ba7ef958cc7606d6dea3c58fc63252b1
Paweł Nowak <pawel834@gmail.com>
2015-05-09 23:10:12 +0200

    Finished the size indexed vector.

--- src/main/scala/CML/Algebra/Vector.scala
+++ src/main/scala/CML/Algebra/Vector.scala
@@ -5,44 +5,49 @@ import shapeless.ops.nat.ToInt
 
 import scalaz.{Applicative, Zip}
 
-case class Vector[S <: Nat, A] (
+case class Vector[+S <: Nat, +A] (
   val vec: scala.collection.immutable.Vector[A]
 ) extends Traversable[A]  {
   override def foreach[U](f: (A) => U): Unit = vec.foreach(f)
 }
 
+class VectorImpl[S <: Nat](implicit size: ToInt[S])
+  extends Zip[({type `T[S]`[a] = Vector[S, a]})#`T[S]`]
+  with Applicative[({type `T[S]`[a] = Vector[S, a]})#`T[S]`]
+  with Concrete[({type `T[S]`[a] = Vector[S, a]})#`T[S]`] {
+  type Type[a] = Vector[S, a]
+
+  override def zip[A, B](a: => Vector[S, A], b: => Vector[S, B]): Vector[S, (A, B)] = new Vector(a.vec.zip(b.vec))
+
+  override def map[A, B](fa: Vector[S, A])(f: (A) => B): Vector[S, B] = Vector[S, B](fa.vec.map(f))
+  override def point[A](a: => A): Vector[S, A] = new Vector(scala.collection.immutable.Vector.fill(dim)(a))
+  override def ap[A, B](fa: => Vector[S, A])(f: => Vector[S, (A) => B]): Vector[S, B] =
+    map(zip(fa, f))(x => x._2(x._1))
+
+  override def zero[F](implicit f: Field[F]): Vector[S, F] = point(f.zero)
+  override def add[F](x: Vector[S, F], y: Vector[S, F])(implicit f: Field[F]): Vector[S, F] =
+    map(zip(x, y))(x => f.add(x._1, x._2))
+  override def sub[F](x: Vector[S, F], y: Vector[S, F])(implicit f: Field[F]): Vector[S, F] =
+    map(zip(x, y))(x => f.sub(x._1, x._2))
+  override def neg[F](x: Vector[S, F])(implicit f: Field[F]): Vector[S, F] = map(x)(f.neg)
+
+  override def mull[F](a: F, v: Vector[S, F])(implicit f: Field[F]): Vector[S, F] = map(v)(f.mul(a, _))
+  override def mulr[F](v: Vector[S, F], a: F)(implicit f: Field[F]): Vector[S, F] = map(v)(f.mul(_, a))
+  override def div[F](v: Vector[S, F], a: F)(implicit f: Field[F]): Vector[S, F] = map(v)(f.div(_, a))
+
+  override def taxicab[F](v: Vector[S, F])(implicit f: Analytic[F]): F = v.map(f.abs).fold(f.zero)(f.add)
+  override def length[F](v: Vector[S, F])(implicit f: Analytic[F]): F = f.sqrt(dot(v, v))
+  override def dot[F](u: Vector[S, F], v: Vector[S, F])(implicit f: Field[F]): F =
+    apply2(u, v)(f.mul).fold(f.zero)(f.add)
+
+  override def dim: Int = size()
+  override def map[F](g: AnalyticMap)(v: Vector[S, F])(implicit f: Analytic[F]): Vector[S, F] = map(v)(x => g(x))
+}
+
 object Vector {
-  def apply[S <: Nat](size: S)(implicit n: ToInt[S]) = {
-    type `Vector[S]`[a] = Vector[S, a]
+  def apply[S <: Nat](implicit size: ToInt[S]) =
+    new VectorImpl[S]
 
-    new Zip[`Vector[S]`]
-      with Applicative[`Vector[S]`]
-      with Concrete[`Vector[S]`] {
-        override def zip[A, B](a: => Vector[S, A], b: => Vector[S, B]): Vector[S, (A, B)] = new Vector(a.vec.zip(b.vec))
-
-        override def map[A, B](fa: Vector[S, A])(f: (A) => B): Vector[S, B] = Vector[S, B](fa.vec.map(f))
-        override def point[A](a: => A): Vector[S, A] = new Vector(scala.collection.immutable.Vector.fill(dim)(a))
-        override def ap[A, B](fa: => Vector[S, A])(f: => Vector[S, (A) => B]): Vector[S, B] =
-          map(zip(fa, f))(x => x._2(x._1))
-
-        override def zero[F](implicit f: Field[F]): Vector[S, F] = point(f.zero)
-        override def add[F](x: Vector[S, F], y: Vector[S, F])(implicit f: Field[F]): Vector[S, F] =
-          map(zip(x, y))(x => f.add(x._1, x._2))
-        override def sub[F](x: Vector[S, F], y: Vector[S, F])(implicit f: Field[F]): Vector[S, F] =
-          map(zip(x, y))(x => f.sub(x._1, x._2))
-        override def neg[F](x: Vector[S, F])(implicit f: Field[F]): Vector[S, F] = map(x)(f.neg)
-
-        override def mull[F](a: F, v: Vector[S, F])(implicit f: Field[F]): Vector[S, F] = map(v)(f.mul(a, _))
-        override def mulr[F](v: Vector[S, F], a: F)(implicit f: Field[F]): Vector[S, F] = map(v)(f.mul(_, a))
-        override def div[F](v: Vector[S, F], a: F)(implicit f: Field[F]): Vector[S, F] = map(v)(f.div(_, a))
-
-        override def taxicab[F](v: Vector[S, F])(implicit f: Analytic[F]): F = v.map(f.abs).fold(f.zero)(f.add)
-        override def length[F](v: Vector[S, F])(implicit f: Analytic[F]): F = f.sqrt(dot(v, v))
-        override def dot[F](u: Vector[S, F], v: Vector[S, F])(implicit f: Field[F]): F =
-          apply2(u, v)(f.mul).fold(f.zero)(f.add)
-
-        override def dim: Int = n()
-        override def map[F](g: AnalyticMap)(v: Vector[S, F])(implicit f: Analytic[F]): Vector[S, F] = map(v)(x => g(x))
-      }
-  }
+  def apply[S <: Nat](n: S)(implicit size: ToInt[S]) =
+    new VectorImpl[S]
 }
--- src/test/scala/AlgebraTest.scala
+++ src/test/scala/AlgebraTest.scala
@@ -1,16 +1,28 @@
 import CML.Algebra.Real._
-import CML.Algebra.{Additive, Additive1, Field, Vector}
+import CML.Algebra._
+import shapeless.ops.nat.ToInt
 import shapeless.{Succ, _0, Nat}
 
 object AlgebraTest extends App {
   def fun[F](x: F)(implicit f: Additive[F]): F = f.add(x, x)
   def fun2[V[_], F](x: V[F])(implicit s: Additive1[V], f: Field[F]): V[F] = s.add(x, x)
-
   println(fun[Double](2))
 
-  implicit val i = Vector(Nat(3))
-  val i2 = Vector(Nat(3))
-  val v = i2.point(2.0)
-  val v2 = i.add(v, v)
-  print(fun2(v2))
+  implicit val vec2 = Vector(Nat(2))
+  implicit val vec3 = Vector(Nat(3))
+  val vec2b = Vector(Nat(2))
+
+  println(vec2.dim)
+  println(vec3.dim)
+
+  val u: vec3.Type[Double] = vec3.point(2.0)
+  val v = vec3.mull(3.0, u)
+  println(u)
+  println(v)
+
+  val w = vec2.point(1.0)
+  val r = vec2.add(w, vec2b.point(3.0))
+  println(w)
+
+  println(fun2[vec2.Type, Double](w))
 }
