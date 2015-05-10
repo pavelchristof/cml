5763419310509c3384d5af65e14e4558a7bdbd63
Paweł Nowak <pawel834@gmail.com>
2015-05-11 00:03:34 +0200

    Cleanup and document model.

--- src/main/scala/cml/models/Model.scala
+++ src/main/scala/cml/models/Model.scala
@@ -2,14 +2,36 @@ package cml.models
 
 import cml.algebra.traits._
 
+/**
+ * A machine learning model, taking some input and transforming it to some output.
+ * @tparam In The input type, parametrized by the numeric type.
+ * @tparam Out The output type, parametrized by the numeric type.
+ */
 trait Model[-In[_], +Out[_]] {
+  /**
+   * The type of model instances.
+   */
   type Type[_] <: Serializable
 
+  /**
+   * Model instance is required to be a concrete vector space.
+   */
   implicit val concrete: Concrete[Type]
-  val normed: Normed[Type] = concrete
-  val linear: Linear[Type] = normed
-  val additive1: Additive1[Type] = linear
 
+  /**
+   * Applies the model to some input.
+   * @param input The input.
+   * @param model The model instance.
+   * @param f Numeric operations.
+   * @tparam F The numeric type.
+   * @return The output.
+   */
   def apply[F](input: In[F])(model: Type[F])(implicit f: Analytic[F]): Out[F]
-  def fill[F](x: => F)(implicit f: Field[F]): Type[F] = concrete.tabulate(_ => x)
+
+  /**
+   * Creates a new model instance filled with some value.
+   * @param x Value the model parameters will be initialized with.
+   * @tparam F The number type.
+   */
+  def fill[F](x: => F): Type[F] = concrete.point(x)
 }
