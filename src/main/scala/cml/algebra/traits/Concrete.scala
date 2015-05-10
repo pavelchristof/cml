f42e5e7cabaefd216fa37382c37452f9b408fbf8
Paweł Nowak <pawel834@gmail.com>
2015-05-10 23:25:31 +0200

    Removed analytic function from Concrete.

--- src/main/scala/cml/algebra/traits/Concrete.scala
+++ src/main/scala/cml/algebra/traits/Concrete.scala
@@ -78,25 +78,4 @@ trait Concrete[V[_]] extends Normed[V] with Applicative[V] with Foldable[V] {
 
   override def foldRight[A, B](fa: V[A], z: => B)(f: (A, => B) => B): B =
     (0 until dim()).map(i => index(fa)(i)).foldRight(z)(f(_, _))
-
-  def abs[F](x: V[F])(implicit f: Analytic[F]): V[F] = pointwise(AnalyticMap.abs)(x)
-  def signum[F](x: V[F])(implicit f: Analytic[F]): V[F] = pointwise(AnalyticMap.signum)(x)
-
-  def exp[F](x: V[F])(implicit f: Analytic[F]): V[F] = pointwise(AnalyticMap.exp)(x)
-  def log[F](x: V[F])(implicit f: Analytic[F]): V[F] = pointwise(AnalyticMap.log)(x)
-  def sqrt[F](x: V[F])(implicit f: Analytic[F]): V[F] = pointwise(AnalyticMap.sqrt)(x)
-
-  def sin[F](x: V[F])(implicit f: Analytic[F]): V[F] = pointwise(AnalyticMap.sin)(x)
-  def cos[F](x: V[F])(implicit f: Analytic[F]): V[F] = pointwise(AnalyticMap.cos)(x)
-  def tan[F](x: V[F])(implicit f: Analytic[F]): V[F] = pointwise(AnalyticMap.tan)(x)
-
-  def asin[F](x: V[F])(implicit f: Analytic[F]): V[F] = pointwise(AnalyticMap.asin)(x)
-  def acos[F](x: V[F])(implicit f: Analytic[F]): V[F] = pointwise(AnalyticMap.acos)(x)
-  def atan[F](x: V[F])(implicit f: Analytic[F]): V[F] = pointwise(AnalyticMap.atan)(x)
-
-  def sinh[F](x: V[F])(implicit f: Analytic[F]): V[F] = pointwise(AnalyticMap.sinh)(x)
-  def cosh[F](x: V[F])(implicit f: Analytic[F]): V[F] = pointwise(AnalyticMap.cosh)(x)
-  def tanh[F](x: V[F])(implicit f: Analytic[F]): V[F] = pointwise(AnalyticMap.tanh)(x)
-
-  def sigmoid[F](x: V[F])(implicit f: Analytic[F]): V[F] = pointwise(AnalyticMap.sigmoid)(x)
 }
