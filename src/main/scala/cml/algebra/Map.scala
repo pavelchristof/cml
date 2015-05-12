1ee829beb41f940cfbbeb46542c7b25987f6a06f
Paweł Nowak <pawel834@gmail.com>
2015-05-12 23:28:02 +0200

    Moved SubMap into Map.

--- src/main/scala/cml/algebra/Map.scala
+++ src/main/scala/cml/algebra/Map.scala
@@ -2,6 +2,7 @@ package cml.algebra
 
 import cml.Enumerate
 import cml.algebra.traits._
+import scalaz.Monoid
 import scalaz.Scalaz._
 
 object Map {
@@ -67,6 +68,43 @@ object Map {
      */
     override def restrict[A](v: Map[K, A])(implicit field: Field[A]): Concrete[({type T[A] = Map[K, A]})#T] =
       new SubMap[K](v.keySet)
+  }
+
+  class SubMap[K](keys: Set[K]) extends Concrete[({type T[A] = Map[K, A]})#T] {
+    override type Index = K
+
+    /**
+     * The (finite) dimension of this vector space.
+     */
+    val dimFin: BigInt = keys.size
+
+    /**
+     * The index must be recursively enumerable.
+     */
+    override def enumerateIndex: Enumerate[Index] = Enumerate.seq(keys.toSeq)
+
+    /**
+     * Construct a vector from coefficients of the basis vectors.
+     */
+    override def tabulate[A](h: (Index) => A): Map[K, A] = keys.map(k => (k, h(k))).toMap
+
+    /**
+     * Find the coefficient of a basis vector.
+     */
+    override def index[A](v: Map[K, A])(i: Index): A = v(i)
+
+    override def map[A, B](x: Map[K, A])(f: (A) => B): Map[K, B] = x.mapValues(f)
+
+    override def point[A](a: => A): Map[K, A] = tabulate(_ => a)
+
+    override def ap[A, B](x: => Map[K, A])(f: => Map[K, (A) => B]): Map[K, B] =
+      x.intersectWith(f){ case (y, g) => g(y) }
+
+    override def foldMap[A, B](v: Map[K, A])(op: (A) => B)(implicit m: Monoid[B]): B =
+      v.mapValues(op).values.fold(m.zero)(m.append(_, _))
+
+    override def foldRight[A, B](v: Map[K, A], z: => B)(op: (A, => B) => B): B =
+      v.values.foldRight(z)(op(_, _))
   }
 
   def additive1[K]: Additive1[({type T[A] = Map[K, A]})#T] = new MapAdditive1[K]
